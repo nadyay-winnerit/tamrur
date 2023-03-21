c3f752e217f7f2a9a3544b0ff71c331508aff4ac
efrat <efratnti@gmail.com>
2023-03-21 10:22:30 +0200

    searchRow() TableWe

--- src/tests/MyTest.java
+++ src/tests/MyTest.java
@@ -1,11 +1,14 @@
 package tests;
 
 import infra.Browser;
+import objects.TableWe;
 import objects.pages.LoginPageData;
 import org.junit.AfterClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pages.LoginPage;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -13,7 +16,7 @@ public class MyTest {
 
     @Test
     public void test_1() throws InterruptedException {
-
+        TableWe tableWe = new TableWe("table", By.tagName("tbody"));
         LoginPage loginPage = new LoginPage();
         LoginPageData loginPageData = new LoginPageData();
         loginPageData.setUserNameUi("auto@mation.com");
@@ -21,6 +24,14 @@ public class MyTest {
         loginPageData.setRememberCheckBoxUi(true);
         loginPage.fillPage(loginPageData);
         loginPage.connectionButtonClick();
+        Thread.sleep(2002);
+        //click on תיקים רפואיים
+        Browser.driver().findElement(By.cssSelector("a.nav-link.active[ng-reflect-router-link='/home/dashboard']")).click();
+        Thread.sleep(2022);
+        WebElement row = Browser.driver().findElement(By.xpath("/html[1]//table[1]/tbody[1]/tr[4]"));
+        //int i = tableWe.searchRowIndex(tableWe.getElement().findElements(By.tagName("tr")).get(4));
+        int i = tableWe.searchRowIndex(row);
+        System.out.println(i);
 
     }
 
