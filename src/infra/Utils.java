428c52b1f2c3e7f08b6f1c385d3f8f4ece74ec85
saraba9 <s0583265049@gmail.com>
2023-04-24 10:17:58 +0300

    e.printStackTrace();

--- src/infra/Utils.java
+++ src/infra/Utils.java
@@ -1,13 +1,12 @@
 package infra;
 
 public class Utils {
-
     public static void sleep(int second){
         int millisecond=second*1000;
         try {
             Thread.sleep(millisecond);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
